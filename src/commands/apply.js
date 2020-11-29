1cd2b75860ff8b3a8a85e2a35238ec80e1ae5b36
Elon Salfati <elon@salfati.io>
2020-11-29 15:27:43 +0200

    updated cli tools

--- src/commands/apply.js
+++ src/commands/apply.js
@@ -1,20 +1,20 @@
 const {Command, flags} = require('@oclif/command')
 
-class HelloCommand extends Command {
+class ApplyCommand extends Command {
   async run() {
-    const {flags} = this.parse(HelloCommand)
+    const {flags} = this.parse(ApplyCommand)
     const name = flags.name || 'world'
     this.log(`hello ${name} from ./src/commands/hello.js`)
   }
 }
 
-HelloCommand.description = `Describe the command here
+ApplyCommand.description = `Describe the command here
 ...
 Extra documentation goes here
 `
 
-HelloCommand.flags = {
+ApplyCommand.flags = {
   name: flags.string({char: 'n', description: 'name to print'}),
 }
 
-module.exports = HelloCommand
+module.exports = ApplyCommand
