d3ec2e82f2bb79f4c7e2fe2ed8553b8a62f511d0
Matan Yechiel <matan@rele.ai>
2021-02-10 14:44:57 +0200

    error hanlding

--- src/commands/app/list.js
+++ src/commands/app/list.js
@@ -8,31 +8,38 @@ class ListCommand extends BaseCommand {
   async run() {
     cli.ux.action.start("Pull Apps")
 
-    // resolve access token
-    const accessToken = await this.accessToken
+    try {
+      // resolve access token
+      const accessToken = await this.accessToken
 
-    const { user } = await this.user
+      const { user } = await this.user
 
-    // init apps client
-    const appsClient = new AppsClient(accessToken.id_token)
+      // init apps client
+      const appsClient = new AppsClient(accessToken.id_token)
 
-    // set conditions array
-    const conditions = [
-      this._toBytes(["org", "==", "global"]),
-      this._toBytes(["org", "==", user.orgs[0]])
-    ]
+      // set conditions array
+      const conditions = [
+        this._toBytes(["org", "==", "global"]),
+        this._toBytes(["org", "==", user.orgs[0]])
+      ]
 
-    // collect promises apps list with "or" condition of org_id
-    const promises = conditions.map(async cond => {
-      const { apps = [] } = await appsClient.list([cond])
-      return apps
-    })
+      // collect promises apps list with "or" condition of org_id
+      const promises = conditions.map(async cond => {
+        const { apps = [] } = await appsClient.list([cond])
+        return apps
+      })
 
-    // resolve app list promises
-    const appsRecords = await flatten(await Promise.all(promises))
-    this.log("Apps pulled successfuly.")
+      // resolve app list promises
+      const appsRecords = flatten(await Promise.all(promises))
+      this.log("Apps pulled successfuly.")
 
-    return appsRecords
+      return appsRecords
+    } catch(error) {
+      console.error(error)
+      cli.ux.action.stop("failed")
+      this.error("unable to list apps")
+      return
+    }
   }
 }
 
