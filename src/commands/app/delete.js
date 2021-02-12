4334db245d2106f00b7e2dcc985480cab12c0ab9
Elon Salfati <elon@salfati.io>
2021-02-12 22:23:15 +0200

    added app:delete command

--- src/commands/app/delete.js
+++ src/commands/app/delete.js
@@ -1,22 +1,54 @@
-const {Command, flags} = require('@oclif/command')
+const cli = require("cli-ux")
+const BaseCommand = require("../../utils/base-command")
+const { AppsClient } = require("../../../lib/components")
 
-class DeleteCommand extends Command {
+/**
+ * Delete an application from RELE.AI. Only applications
+ * that are related to the user's organization can be deleted.
+ */
+class DeleteCommand extends BaseCommand {
+  // define the command arguments
+  static args = [
+    {
+      name: "key",
+      required: true,
+      description: "App selector key."
+    }
+  ]
+
+  /**
+   * Execute the delete command
+   */
   async run() {
-    this.log(`get`)
+    // destruct the key from the arguments
+    const { key } = this.args
+
+    // start cli spinner
+    cli.ux.action.start(`Deleting application ${key}`)
+
+    // try to delete the application
+    try {
+      // resolve access token and user
+      const [accessToken, { user }] = await Promise.all([this.accessToken, this.user])
+
+      // init apps client
+      const client = new AppsClient(user, accessToken)
+
+      // delete application by key
+      await client.deleteByKey(key)
+
+      // stop spinner
+      cli.ux.action.stop()
+    } catch (err) {
+      cli.ux.action.stop("failed")
+      this.error(`Unable to delete application ${key}.\n${err}`)
+    }
   }
 }
 
-DeleteCommand.description = `Apply a set of configurations to RELE.AI App
+DeleteCommand.description = `Delete an application from RELE.AI by the application key.
 ...
-Please read more about the configuration files in the github repository docs.
+Additional information about the app:delete command can be found at https://doc.rele.ai/guide/cli-config.html#rb-app-delete
 `
-
-DeleteCommand.flags = {
-  config: flags.string({
-    char: "c",
-    description: "A path to the configuration file",
-    required: true
-  })
-}
 
 module.exports = DeleteCommand
