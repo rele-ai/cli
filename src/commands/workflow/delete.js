e428a827adfef2c38eba01927e0ed68045ee3e24
Elon Salfati <elon@salfati.io>
2021-02-14 08:43:07 +0200

    added workflow:delete

--- src/commands/workflow/delete.js
+++ src/commands/workflow/delete.js
@@ -1,22 +1,54 @@
-const {Command, flags} = require('@oclif/command')
+const cli = require("cli-ux")
+const BaseCommand = require("../../utils/base-command")
+const { WorkflowsClient } = require("../../../lib/components")
 
-class DeleteCommand extends Command {
+/**
+ * Delete a workflow from RELE.AI. Only workflows
+ * that are related to the user's organization can be deleted.
+ */
+class DeleteCommand extends BaseCommand {
+  // define the command arguments
+  static args = [
+    {
+      name: "key",
+      required: true,
+      description: "Workflow selector key."
+    }
+  ]
+
+  /**
+   * Execute the delete command
+   */
   async run() {
-    this.log(`get`)
+    // destruct the key from the arguments
+    const { key } = this.args
+
+    // start cli spinner
+    cli.ux.action.start(`Deleting workflow ${key}`)
+
+    // try to delete the workflow
+    try {
+      // resolve access token and user
+      const [accessToken, { user }] = await Promise.all([this.accessToken, this.user])
+
+      // init workflows client
+      const client = new WorkflowsClient(user, accessToken)
+
+      // delete workflow by key
+      await client.deleteByKey(key)
+
+      // stop spinner
+      cli.ux.action.stop()
+    } catch (err) {
+      cli.ux.action.stop("failed")
+      this.error(`Unable to delete workflow ${key}.\n${err}`)
+    }
   }
 }
 
-DeleteCommand.description = `Apply a set of configurations to RELE.AI App
+DeleteCommand.description = `Delete a workflow from RELE.AI by the workflow key.
 ...
-Please read more about the configuration files in the github repository docs.
+Additional information about the workflow:delete command can be found at https://doc.rele.ai/guide/cli-config.html#rb-workflow-delete
 `
-
-DeleteCommand.flags = {
-  config: flags.string({
-    char: "c",
-    description: "A path to the configuration file",
-    required: true
-  })
-}
 
 module.exports = DeleteCommand
