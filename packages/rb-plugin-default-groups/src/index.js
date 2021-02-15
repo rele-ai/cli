230527916f0ac721dcfad105b6c2a16e062db25d
Elon Salfati <elon@salfati.io>
2021-02-15 09:43:20 +0200

    moved to comments

--- packages/rb-plugin-default-groups/src/index.js
+++ packages/rb-plugin-default-groups/src/index.js
@@ -1,12 +1,72 @@
 const { declare } = require("@releai/helper-plugin-utils")
 
 /**
+ * Destruct given groups into matching objects.
+ *
+ * @param {object} payload - Command payload.
+ * @returns {Array.<object>}
+ */
+const destructGroups = (payload) => {
+	if (payload.configs) {
+		return payload.configs.flatMap((doc) => {
+			switch (doc.type) {
+				case "app_action":
+					return destructAppActions(doc)
+				case "operation":
+					return destructOperations(doc)
+				default:
+					return doc
+			}
+		})
+	} else {
+		throw new Error("unable to destruct groups. missing payload config")
+	}
+}
+
+/**
+ * Destruct given app actions into matching objects.
+ *
+ * @param {object} doc - Config document.
+ */
+const destructAppActions = (doc) => {
+	console.log("destruct app action", doc)
+}
+
+/**
+ * Destruct given operations into matching objects.
+ *
+ * @param {object} doc - Config document.
+ */
+const destructOperations = (doc) => {
+	console.log("destruct operation", doc)
+}
+
+/**
+ * Group app actions objects into a single config.
+ *
+ * @param {object} payload - Command payload.
+ */
+const groupAppActions = (payload) => {
+	console.log("group app action", payload)
+}
+
+/**
+ * Group operation objects into a single config.
+ *
+ * @param {object} payload - Command payload.
+ */
+const groupOperations = (payload) => {
+	console.log("group operations", payload)
+}
+
+/**
  * Export default plugin callback
  */
 module.exports = declare((api) => {
-	api.translation.list.on("success", (payload) => {
-		console.log("here", payload)
-	})
+	// // handler group destructuring on apply
+	// api.apply.on("load", destructGroups)
 
-	return api
+	// // handler group structuing on list
+	// api.app_action.list.on("load", groupAppActions)
+	// api.operation.list.on("load", groupOperations)
 })
