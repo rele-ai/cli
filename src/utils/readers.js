922172b948b89ed362a8226dbf3cd792259f27fa
Matan Yechiel <matan@rele.ai>
2021-03-02 09:55:46 +0200

    support-folder-apply

--- src/utils/readers.js
+++ src/utils/readers.js
@@ -8,9 +8,40 @@ const yaml = require("js-yaml")
  * @returns {object} - Config file data.
  */
 module.exports.readConfig = (path) => {
-  // read the file
-  const file = fs.readFileSync(path, "utf-8")
+  // define files list
+  let files = []
+
+  // check if path exists
+  if (fs.existsSync(path)) {
+    // pull path stats
+    const stats = fs.lstatSync(path)
+
+    // check stats source
+    if (stats.isDirectory()) {
+      // get files name in folder
+      const filenames = fs.readdirSync(path).filter(fileName => fileName.endsWith(".yaml"))
+
+      // remove back-slash if exists
+      const formattedPath = path.endsWith("/") ? path.slice(0, -1) : path
+
+      // add files to list
+      filenames.forEach(file => {
+        files.push(fs.readFileSync(`${formattedPath}/${file}`, "utf-8"))
+      })
+
+    } else if (stats.isFile()) {
+      // read the file
+      files.push(fs.readFileSync(path, "utf-8"))
+    } else {
+      throw new Error("unknown path given.")
+    }
+  } else {
+    throw new Error("path is not exists.")
+  }
+
+  // file to upload
+  const mergedFile = files.join("\n---\n")
 
   // parse yaml to object and return to user
-  return yaml.loadAll(file)
+  return yaml.loadAll(mergedFile)
 }
