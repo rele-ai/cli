fe67056f9c020601ec69406498a5964637cc29f3
Matan Yechiel <matan@rele.ai>
2021-05-06 09:23:03 +0300

    warn on validation

--- packages/cli/src/utils/parser.js
+++ packages/cli/src/utils/parser.js
@@ -15,12 +15,16 @@ const { loadConfNextOperations, loadDocNextOperations } = require("./index")
 * @returns {object} doc - Firestore doc representation.
 */
 module.exports.confToDoc = (confType, conf, { apps, appActions, workflows, versions, user, client = null } = {}) => {
-  // validate configurations before apply
-  const validate = client.validate(conf)
-  if (validate !== null) {
-    const falidKeys = Object.keys(validate.validation || {})
-    debugError(validate)
-    throw new Error(`unable to validate configurations before apply.\n The following fields are not set correctly: ${falidKeys.join(", ")}\n For more information: https://docs.rele.ai/`)
+  if (client) {
+    // validate configurations before apply
+    const validate = client.validate(conf)
+    if (validate !== null) {
+      const falidKeys = Object.keys(validate.validation || {})
+      debugError(validate)
+      throw new Error(`unable to validate configurations before apply.\n The following fields are not set correctly: ${falidKeys.join(", ")}\n For more information: https://docs.rele.ai/`)
+    }
+  } else {
+    console.warn("Configurations validation was not executed. Please pass the relevant client object.")
   }
 
   // parse from conf to doc
