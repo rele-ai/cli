c116c4fc8ac61d7dc6078d8549e386c37d1d0bff
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-09-13 14:12:06 +0300

    CU-k025ev - adding apply test with token combination

---
+++ packages/cli/test/commands/apply.test.js
@@ -0,0 +1,22 @@
+const {expect, test} = require('@oclif/test')
+const path = require('path')
+
+process.env.NODE_ENV = "development"
+const refresh_token = "ACzBnCgyZafR_GBdzEhwtX2p8c9HtTYQiYhljVjqP-mjpvUD5JXeYcZHzo8GETvmTdMjvtDoTC5GdInt20Q3nfXAmaiFwj0ofRs4mdOn_3rEsTbNlaLSgS2bYdfwJi2X5XmLdsfgEEEWuE95O9ZXXG0c-20JEexkniLppmwna2FR4Qw-MqxFxp2hL6gjg50eS8FDqQvYOWOcUiFNu00K2LnIn6iEammClVUZZA1AjlQs14dWd1vAKFs"
+
+describe('Testing apply and delete commands with the tokens combination', () => {
+
+  test
+  .stdout()
+  .command(['apply','-f',path.join(__dirname,'example.yaml'),'-T',refresh_token])
+  .it('Should apply configuration file using the refresh token', ctx => {
+    expect(ctx.stdout).to.equal('')
+  })
+
+  test
+  .stdout()
+  .command(['app:delete','example_app','-T',refresh_token])
+  .it('Should delete configuration file using the refresh token', ctx => {
+    expect(ctx.stdout).to.equal('')
+  })
+})
