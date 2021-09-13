74daf2fbe18552f151fb9414d9e8bc74c1ed0936
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-09-13 14:36:12 +0300

    CU-k025ev - change apply test . deleting the creds to be sure the token provided is beeing used

--- packages/cli/test/commands/apply.test.js
+++ packages/cli/test/commands/apply.test.js
@@ -1,10 +1,18 @@
+const BaseCommand =  require('../../src/utils/base-command')
 const {expect, test} = require('@oclif/test')
 const path = require('path')
-
-process.env.NODE_ENV = "development"
+const fs = require('fs')
 const refresh_token = "ACzBnCgyZafR_GBdzEhwtX2p8c9HtTYQiYhljVjqP-mjpvUD5JXeYcZHzo8GETvmTdMjvtDoTC5GdInt20Q3nfXAmaiFwj0ofRs4mdOn_3rEsTbNlaLSgS2bYdfwJi2X5XmLdsfgEEEWuE95O9ZXXG0c-20JEexkniLppmwna2FR4Qw-MqxFxp2hL6gjg50eS8FDqQvYOWOcUiFNu00K2LnIn6iEammClVUZZA1AjlQs14dWd1vAKFs"
 
+process.env.NODE_ENV = "development"
+
 describe('Testing apply and delete commands with the tokens combination', () => {
+
+  //remove creds.json if exists
+  if(fs.existsSync(BaseCommand.CREDS_PATH)){
+    fs.unlinkSync(BaseCommand.CREDS_PATH)
+    console.log("creds.json is deleted")
+  }
 
   test
   .stdout()
