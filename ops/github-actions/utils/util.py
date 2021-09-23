b450435e086cc2d9c13533a1cc631dae2f997b67
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-09-23 15:25:35 +0300

    update

--- ops/github-actions/utils/util.py
+++ ops/github-actions/utils/util.py
@@ -23,8 +23,10 @@ def run_the_yaml_command(dir, id):
   for command in config['development']['scripts']['test']:
     print(command)
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, shell=True)
+    output, e = p.communicate()
+    print(output)
     if p.wait() != 0:
-      raise Exception()
+      raise Exception(e)
 
 def ch_dir(dir, id):
   '''
