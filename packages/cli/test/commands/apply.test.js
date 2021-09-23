04e726bab9108fe899cee95387f675cb72d5b644
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-09-23 15:27:58 +0300

    update

--- packages/cli/test/commands/apply.test.js
+++ packages/cli/test/commands/apply.test.js
@@ -18,15 +18,18 @@ describe('Testing apply and delete commands with the tokens combination', () => 
     const payload ={ "email": "qa@rele.ai", "password": "Aa123456!", "returnSecureToken" : true }
     const { data } = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.RELEAI_FS_API_KEY}`, payload, headers)
     rToken = data.refreshToken
+    console.log(rToken)
   })
 
 	afterEach(() => jest.restoreAllMocks());
 
   it('should apply example.yaml', async () => {
+    console.log("the first test")
 		await ApplyCommand.run(['-f',path.join(__dirname,'example.yaml'), '-T',rToken])
 	});
 
   it("should delete example_app", async () => {
+    console.log("the second test")
     await DeleteCommand.run(['example_app','-T',rToken])
   })
 })
