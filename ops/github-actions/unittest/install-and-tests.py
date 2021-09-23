90dd47e23f7160f9cb049249bd6006b4d7aa8064
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-09-23 14:34:41 +0300

    unitesting.yml was changed

--- ops/github-actions/unittest/install-and-tests.py
+++ ops/github-actions/unittest/install-and-tests.py
@@ -6,6 +6,18 @@ def main():
   1. Runs write_dependencies function
   2. Run run_the_yaml_command, get "dir" name on releai repo, this value come from outside
   '''
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
+  print("saar ha geysssasdsadsadsad")
   releai.run_the_yaml_command("packages", os.environ['PACKAGE'])
 
 if __name__ == '__main__':
