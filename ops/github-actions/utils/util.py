f91d8c349640e4f2b5891cd6d1d3ce14d239244a
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-09-23 14:42:51 +0300

    unitesting.yml was changed

--- ops/github-actions/utils/util.py
+++ ops/github-actions/utils/util.py
@@ -1,22 +1,23 @@
-# import subprocess, os, yaml
+import subprocess, os, yaml
 
-# path = os.path.join(os.environ['GITHUB_WORKSPACE'])
-# config_envs = {} ## this object outside beacuse of the for loop
+path = os.path.join(os.environ['GITHUB_WORKSPACE'])
+config_envs = {} ## this object outside beacuse of the for loop
 
-# def load_src_yaml(dir, id):
-#   '''
-#   This function load the service yaml file
-#   '''
-#   loaded_yaml = yaml.load(open(f"{path}/{dir}/{id}/releai-config.yaml").read(),Loader=yaml.FullLoader)
-#   return loaded_yaml
+def load_src_yaml(dir, id):
+  '''
+  This function load the service yaml file
+  '''
+  print(dir,id)
+  loaded_yaml = yaml.load(open(f"{path}/{dir}/{id}/releai-config.yaml").read(),Loader=yaml.FullLoader)
+  return loaded_yaml
 
 def run_the_yaml_command(dir, id):
   '''
   Load and run each test command under "releai-config.yaml"
   '''
   print("1")
-  # config = load_src_yaml(dir, id)
-  # print("2")
+  config = load_src_yaml(dir, id)
+  print("2")
   # ch_dir(dir, id)
   # print("3")
   # for command in config['development']['scripts']['test']:
