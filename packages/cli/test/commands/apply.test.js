c103bc5554107aa92d65d348bdfd9e5083ea2795
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-09-29 11:16:31 +0300

    CU-m2ay9f - tests was fixed

--- packages/cli/test/commands/apply.test.js
+++ packages/cli/test/commands/apply.test.js
@@ -14,7 +14,6 @@ describe('Testing apply and delete commands with the tokens combination', () => 
     const headers = {
       'Content-Type': 'application/json',
     };
-    //TODO - Put here a valid rele.ai email, and a valid password
     const payload ={ "email": "qa@rele.ai", "password": "Aa123456!", "returnSecureToken" : true }
     const { data } = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.RELEAI_FS_API_KEY}`, payload, headers)
     rToken = data.refreshToken
@@ -22,10 +21,10 @@ describe('Testing apply and delete commands with the tokens combination', () => 
   })
 
   it('should apply example.yaml', async () => {
-		// await ApplyCommand.run(['-f',path.join(__dirname,'example.yaml'), '-T',rToken])
+		await ApplyCommand.run(['-f',path.join(__dirname,'example.yaml'), '-T',rToken])
 	});
 
   it("should delete example_app", async () => {
-    // await DeleteCommand.run(['example_app','-T',rToken])
+    await DeleteCommand.run(['example_app','-T',rToken])
   })
 })
