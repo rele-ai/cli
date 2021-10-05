f53dd51d909ffa8ca4cd6d44b897c8bdad87c449
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-10-05 11:51:40 +0300

    CU-1m8b7g6 - remove uneccesery lines in inrement_version.py

--- ops/github-actions/master/increment_version.py
+++ ops/github-actions/master/increment_version.py
@@ -6,9 +6,6 @@ def main():
   '''
   This is main function made for replace the image on k8s
   '''
-  # write the keys
-  # releai.bash_subprocess("ops/github-actions/utils", "secrets.sh")
-  # increment the version by 1 every time
   new_version = util.increment_version(os.environ['PACKAGE'])
   # just print
   print(f"this is the new version of {os.environ['PACKAGE']}: ", new_version)
