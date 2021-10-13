474ef0a595795a40377dc52dcb7b437aadaa4986
gal-sh <galshalom@Gals-MacBook-Pro.local>
2021-10-13 11:12:15 +0300

    CU-1mtgtrk - minor changes

--- packages/cli/src/commands/auth/login-ci.js
+++ packages/cli/src/commands/auth/login-ci.js
@@ -7,9 +7,10 @@ const e = require("express")
 const credsPath = path.join(os.homedir(), ".rb/creds.json")
 const  emitter  = require("../../utils/loginEvent")
 
-const printCreds = () => {
-  let creds = require(credsPath)
-  console.log(`\nSuccess! Use this token to login on a CI server:\n\n${creds.refresh_token}\n\nExample: rb deploy -t ${creds.refresh_token}\n\n`)
+const printCreds = (refresh_token) => {
+  if(!refresh_token)
+    refresh_token = require(credsPath).refresh_token
+  console.log(`\nSuccess! Use this token to login on a CI server:\n\n${refresh_token}\n\nExample: rb deploy -t ${refresh_token}\n\n`)
 }
 // login command execution
 class LoginCICommand extends BaseCommand {
@@ -17,20 +18,15 @@ class LoginCICommand extends BaseCommand {
    * Execute the login command
    */
 async run() {
-    try {
       // the emitter handle exit emit when the user is authorized the request in the browser.
       emitter.on('exit',printCreds)
-
-      if (fs.existsSync(credsPath) && require(credsPath).refresh_token) {
-        printCreds()
+      let refresh_token
+      if (fs.existsSync(credsPath) && (refresh_token = require(credsPath)?.refresh_token)) {
+        printCreds(refresh_token)
       } else {
         LoginCommand.run([])
       }
-
-    } catch(err) {
-        LoginCommand.run([])
     }
-  }
 }
 // command description
 LoginCICommand.description = `Manage the authorization session to RELE.AI
