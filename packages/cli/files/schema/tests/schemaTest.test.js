7dfb4771abb65c78438b5b7cab583e8f7b35e5bf
Elon Salfati <elon@salfati.io>
2021-11-03 09:58:44 +0200

    changes to schema tests

--- packages/cli/files/schema/tests/schemaTest.test.js
+++ packages/cli/files/schema/tests/schemaTest.test.js
@@ -1,11 +1,7 @@
 const Ajv = require("ajv")
-const env = Ajv({
-  coerceTypes: true,
-  useDefaults: true,
-  removeAdditional: true,
-})
 const fs = require("fs")
 const path = require("path")
+const ajvFormats = require("ajv-formats")
 
 /**
  * Reload the schema and validate the json object
@@ -14,6 +10,17 @@ const path = require("path")
  * @param {object} mockData schema to be tested
  */
 const applySchema = (schemaName, schema, mockData = {}) => {
+  // init ajv
+  const env = Ajv({
+    coerceTypes: true,
+    useDefaults: true,
+    removeAdditional: true,
+  })
+
+  // apply ajv formats
+  ajvFormats(env)
+
+  // define base schema
   let data = {
     "selector": {
       "workflow": ["test"],
@@ -24,24 +31,28 @@ const applySchema = (schemaName, schema, mockData = {}) => {
     "type": "Operation",
     ...mockData
   }
+
+  // load schema and append to ajv
   const schemaObj = JSON.parse(fs.readFileSync(path.join(__dirname, `../${schema}`), 'utf8'))
   env.addSchema(schemaObj, schemaName)
+
+  // return validation result
   return env.getSchema(schemaName)(data)
 }
 
 describe("Testing the schemas", () => {
 
-  test("Should success validating the operation Schema", () => {
+  test("Should success validating the operation Schetruema", () => {
     let mockOperation = {
       "input": {
         "redis_functions": [ { "path": "test", "payload" : { "data": "33", "type": "redis" }}]
       }
     }
-    expect(applySchema('operation', 'operations.json', mockOperation)).toBe(null)
+    expect(applySchema('operation', 'operations.json', mockOperation)).toBe(true)
   })
 
   test("Should success validating the operation Schema", () => {
-    expect(applySchema('operation', 'operations.json')).toBe(null)
+    expect(applySchema('operation', 'operations.json')).toBe(true)
   })
 
   test("Should failed validating the operation Schema", () => {
@@ -50,7 +61,7 @@ describe("Testing the schemas", () => {
         "redis_functions": ["d", { "data": "", "type": "gal" }]
       }
     }
-    expect(applySchema('operation', 'operations.json', mockOperation)).not.toBe(null)
+    expect(applySchema('operation', 'operations.json', mockOperation)).toBe(false)
   })
 
   test("Should success validating the operation Schema", () => {
@@ -60,6 +71,6 @@ describe("Testing the schemas", () => {
         "type": "5"
       }
     }
-    expect(applySchema('operation', 'operations.json', mockOperation)).toBe(null)
+    expect(applySchema('operation', 'operations.json', mockOperation)).toBe(true)
   })
 })
